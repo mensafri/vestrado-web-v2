3a614cdc4888278ed71824fb1132cdbf46a9a692
Safri Jawad <safrijawad1245@gmail.com>
2024-12-03 13:32:19 +0700

    feat: ib history page

--- app/ib-panel/history/page.tsx
+++ app/ib-panel/history/page.tsx
@@ -1,5 +1,29 @@
+import AccountManager from "@/components/AccountManager";
+import AccountDetailsMini from "@/components/AccountsDetailsMini";
+import { fullTransactionsHistoryColumns } from "@/components/full-transaction-history/columns";
+import { FullTransactionsHistoryTable } from "@/components/full-transaction-history/data-table";
+import QuickAccess from "@/components/QuickAccess";
+import { transactionHistoryData } from "@/lib/data";
+
 const IbHistoryPage = () => {
-	return <div>IbHistoryPage</div>;
+	return (
+		<div className="flex flex-col md:flex-row w-full md:justify-between p-4 md:space-x-4 space-y-4 md:space-y-0">
+			<div className="w-full md:w-2/3 flex flex-col gap-4">
+				<div className="overflow-x-hidden">
+					<FullTransactionsHistoryTable
+						columns={fullTransactionsHistoryColumns}
+						data={transactionHistoryData}
+						pageSize={8}
+					/>
+				</div>
+			</div>
+			<div className="w-full md:w-1/3 flex flex-col gap-4 items-center">
+				<AccountDetailsMini />
+				<QuickAccess />
+				<AccountManager />
+			</div>
+		</div>
+	);
 };
 
 export default IbHistoryPage;
